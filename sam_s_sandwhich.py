1eac03d98760338c2d0d954b79df5b29a19f926c
ammarhezam08 <ammarh.24211@obhs.school.nz>
2025-10-08 09:28:45 +1300

    6th commit

--- sam_s_sandwhich.py
+++ sam_s_sandwhich.py
@@ -1,3 +1,14 @@
+#This program will allow the user to enter in a name
+def force_name(message,lower,upper):
+    while True: #this is an infinite loop that will only break if only a valid name is entered
+        name=str(input(message)).title()
+        if len(name)>=lower and len(name)<=upper and name.isalpha():
+            print("The name is valid")
+            break #this loop breaks if the above conditions are met
+        else:
+            print("Invalid name")
+    return name #a valid name is returned to the variable that called the function
+
 def bread_selection():
     bread_list=["White","Brown","Italian","Granary"]
     count=0
@@ -35,21 +46,25 @@ def salad_selection():
     while count < len(vegetable_list):
         print(count+1," ",vegetable_list[count])
         count +=1
-    vegetable_selected=int(input("Which type of vegetable did you choose? Enter a number: "))
-    i = 0
-    #hahawhile True:
-        #if vegetable_selected == i + 1:
-            
-
-            #break
-#hahaha
+    print ("Press ENTER when you have finsished choosing your salads")
+    salads_added = "" #Set to nothing
+    salad_type = " " #adds an wmpty string
+    
+    while salad_type != "": 
+        salad_type = input("what number salad do you want? \n1-Lettuce \n2-Tomato \n3-Cucumber \n4-Onions")
+        if salad_type != "": #if you don't press to enter it will prompt you to enter a prompt
+            salad_type = int(salad_type)
+            salads_added = salads_added + " " + vegetable_list[salad_type-1]
+    return salads_added.strip()
+    
 #main program
 print("Welcome to Sam's Sandwhich Shop")
 bread_choice=bread_selection() #creating a variable that calls up the bread function and returns their choice
 cheese_choice=cheese_selection()
 meat_choice=meat_selection()
-vegetable_choice=salad_selection()
+salad_choice=salad_selection()
+first_name = force_name("Enter your first name")
 print(f"Your selected bread: {bread_choice}")
 print(f"Your selected cheese: {cheese_choice}")
 print(f"Your selected meat: {meat_choice}")
-#print(f"Your selected vegetables: {vegetable_choice}")
+print(f"Your selected vegetables: {salad_choice}")
